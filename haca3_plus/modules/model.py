e091aefc673817fae93978a439d70b88d6d90f91
shays15 <122406050+shays15@users.noreply.github.com>
2026-08-29 01:44:04 +0800

    Add debug prints for image and beta shapes
    Added debug print statements for source images and betas shapes.

--- haca3_plus/modules/model.py
+++ haca3_plus/modules/model.py
@@ -439,6 +439,8 @@ class HACA3:
                 subject_id:subject_id + 1,
                 query_idx
             ]
+            print("source_images_stack:", source_images_stack.shape)
+            print("betas_stack:", betas_stack.shape)
     
             query_feature = self.patchifier(
                 query_image
@@ -453,7 +455,9 @@ class HACA3:
                 subject_id:subject_id + 1,
                 query_idx
             ]
-    
+            print("query image:", query_image.shape)
+            print("positive beta:", positive_beta.shape)
+            
             positive_feature = self.patchifier(
                 positive_beta
             ).flatten(start_dim=2)
