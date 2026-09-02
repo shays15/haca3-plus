b239449ef2a732f0970167a063be2233f712e103
shays15 <122406050+shays15@users.noreply.github.com>
2026-09-03 00:04:10 +0800

    Change print statement to show site IDs
    Updated print statement to display site IDs instead of count.

--- haca3_plus/modules/model.py
+++ haca3_plus/modules/model.py
@@ -825,7 +825,7 @@ class HACA3:
         )
         
         batch_size = site_ids.shape[0]
-        print(f"Site IDs: {site_ids.shape[0]}")
+        print(f"Site IDs: {site_ids}")
         
         do_inter_site = (
             is_train
